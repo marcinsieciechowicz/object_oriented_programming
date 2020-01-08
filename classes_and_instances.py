b53d5edbdb694bc3e94ae556174ee26fca104ad0
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2020-01-08 20:56:05 +0100

    continue with learning on oop

--- classes_and_instances.py
+++ classes_and_instances.py
@@ -1,5 +1,8 @@
 class Employee:
 
+    num_of_emps = 0
+    raise_amount = 1.04
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -7,12 +10,30 @@ class Employee:
         # self.email = first.lower() + '.' + last.lower() + '@wp.pl'
         self.email = '{}.{}@wp.pl'.format(first.lower(), last.lower())
 
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
+    def apply_raise(self):
+        self.pay = int(self.pay * Employee.raise_amount)
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+
 emp_1 = Employee('Marcin', 'Sieciechowicz', 200_000)
 emp_2 = Employee('Mikołaj', 'Sieciechowicz', 20_000)
 
-# print(emp_1)
-# print(emp_2)
+emp_str_1 = 'Maciej-Kowalski-7000'
+emp_str_2 = 'Michał-Nowak-1000'
+emp_str_3 = 'Wojciech-Wiśniewski-2000'
 
-print(emp_1.email)
-print(emp_2.email)
+new_emp_1 = Employee.from_string(emp_str_1)
 
+print(new_emp_1.first)
+print(new_emp_1.pay)
---
+++ my_family_exercise.py
@@ -0,0 +1,62 @@
+from enum import Enum
+
+
+class Sex(Enum):
+    MALE: 'm'
+    FEMALE: 'f'
+
+
+class Sieciechowicz_fam_member(object):
+    last = 'Sieciechowicz'
+    num_of_membs = 0
+
+    def __str__(self) -> str:
+        return f'{self.first} {Sieciechowicz_fam_member.last} ({self.sex})'
+
+    def __init__(self, first, sex):
+        self.first = first
+        self.sex = sex
+
+        Sieciechowicz_fam_member.num_of_membs += 1
+
+    lista = ['Marcin', 'male']
+
+    @classmethod
+    def from_string(cls, str):
+        first, sex = str.split()
+        return cls(first, sex)
+
+    @classmethod
+    def from_list(cls, lst):
+        first, sex = lst
+        return cls(first, sex)
+    @classmethod
+    def from_dict(cls, dict):
+        first, sex = dict
+        return cls(first, sex)
+
+s_szuszwol = 'Marcin male'
+szuszwol = Sieciechowicz_fam_member.from_string(s_szuszwol)
+
+lst_Szuszwol = ['Macrin', 'male']
+szuszwol = Sieciechowicz_fam_member.from_list(lst_Szuszwol)
+
+dict = {'Marcin', 'male'}
+dict_szuszwol = dict
+szuszwol = Sieciechowicz_fam_member.from_dict(dict_szuszwol)
+#TODO: create alternative constructor from_dict for a sample dict {first: "Marcin", sex: 'male'}
+
+# szuszwol = Sieciechowicz_fam_member('Marcin', Sex.MALE)
+# Natalia = Sieciechowicz_fam_member('Natalia', Sex.FEMALE)
+# Małgorzata = Sieciechowicz_fam_member('Małgorzata', 'female')
+# Mikołaj = Sieciechowicz_fam_member('Mikołaj', 'male')
+
+# print(szuszwol)
+# print(Natalia)
+# print(Małgorzata)
+# print(Mikołaj)
+
+# print(szuszwol.__dict__)
+# print(object.__dict__)
+
+print(szuszwol)
